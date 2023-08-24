ba7c47c196d4fec0f208be6b20e70b8807f8fff6
Kareem Kwong <kareem.kwong@hey.com>
2023-08-24 12:21:56 -0400

    Add sections (#4)

--- src/components/AddToPouch.jsx
+++ src/components/AddToPouch.jsx
@@ -16,42 +16,36 @@ import mobyCard from '@/images/hero-cards/moby-wrap.png'
 import diaperCard from '@/images/hero-cards/diaper-fund.png'
 import toyCard from '@/images/hero-cards/nursery-toys.png'
 import mainRegistry from '@/images/mainregistry.png'
+import cashfunds from '@/images/cashfunds.png'
 import hero from '@/images/hero.png'
 
 
 
-export function HeroTwo() {
+export function AddToPouch() {
   return (
-    <div className="bg-[#FAEAE7]">
+    <div className="bg-[#FEF6EA]">
     <Container className="relative max-w-7xl lg:pt-32">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+                <div className="max-w-xl">
+                    <Image src={cashfunds} />
+                </div>
                 <div>
-                <h1 className="mx-auto max-w-2xl font-display text-5xl font-medium tracking-tight text-[#FC1938] sm:text-7xl">
-                    Create a beautiful baby registry in minutes
+                <h1 className="mx-auto max-w-2xl font-display text-5xl font-medium tracking-tight text-[#005394] sm:text-7xl">
+                  Set up a cash fund
                 </h1>
-                <p className="mt-6 max-w-lg text-2xl text-[#FC1938] tracking-tight">
-                    One link to curate your baby wish list + help your loved ones
-                    get what you need for your new family.
+                <p className="mt-6 max-w-lg text-2xl text-[#005394] tracking-tight">
+                Let your friends and family contribute and save towards anything from diapers, 
+                doula, childcare, or baby’s future savings accounts.
                 </p>
                 <div className="mt-10 flex gap-x-6">
-                    <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="block rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 sm:text-xl sm:leading-6"
-                    placeholder="babypouch.co/myregistry"
-                    aria-describedby="email-description"
-                    />
-                    <Button href="/" className="bg-[#FC1938] hover:bg-[#FC1938] px-8 py-3 text-[#fff] text-xl">Try now</Button>
                 </div>
                 </div>
-                <div className="max-w-xl">
-                    <Image src={hero} />
 
-                </div>
       </div>
 
       
+      <div className="mt-36 lg:mt-44">
+      </div>
     </Container>
     </div>
   )
--- src/components/SetupCashFund.jsx
+++ src/components/SetupCashFund.jsx
@@ -16,39 +16,32 @@ import mobyCard from '@/images/hero-cards/moby-wrap.png'
 import diaperCard from '@/images/hero-cards/diaper-fund.png'
 import toyCard from '@/images/hero-cards/nursery-toys.png'
 import mainRegistry from '@/images/mainregistry.png'
+import cashfunds from '@/images/cashfunds.png'
 import hero from '@/images/hero.png'
 
 
 
-export function HeroTwo() {
+export function SetupCashFund() {
   return (
-    <div className="bg-[#FAEAE7]">
+    <div className="bg-[#13253B]">
     <Container className="relative max-w-7xl lg:pt-32">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
                 <div>
-                <h1 className="mx-auto max-w-2xl font-display text-5xl font-medium tracking-tight text-[#FC1938] sm:text-7xl">
-                    Create a beautiful baby registry in minutes
+                <h1 className="mx-auto max-w-2xl font-display text-5xl font-medium tracking-tight text-[#FEF2D4] sm:text-7xl">
+                  Set up a cash fund
                 </h1>
-                <p className="mt-6 max-w-lg text-2xl text-[#FC1938] tracking-tight">
-                    One link to curate your baby wish list + help your loved ones
-                    get what you need for your new family.
+                <p className="mt-6 max-w-lg text-2xl text-[#FEF2D4] tracking-tight">
+                Let your friends and family contribute and save towards anything from diapers, 
+                doula, childcare, or baby’s future savings accounts.
                 </p>
                 <div className="mt-10 flex gap-x-6">
-                    <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="block rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 sm:text-xl sm:leading-6"
-                    placeholder="babypouch.co/myregistry"
-                    aria-describedby="email-description"
-                    />
-                    <Button href="/" className="bg-[#FC1938] hover:bg-[#FC1938] px-8 py-3 text-[#fff] text-xl">Try now</Button>
                 </div>
                 </div>
                 <div className="max-w-xl">
-                    <Image src={hero} />
-
+                    <Image src={cashfunds} />
                 </div>
+
+
       </div>
 
       
