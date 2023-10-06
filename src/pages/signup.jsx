90072d25a3c6eac3013dfc43b324cdee436ffb83
Kareem Kwong <kareem.kwong@hey.com>
2023-10-05 23:07:45 -0400

    Add segment

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -6,6 +6,11 @@ import { useRouter } from 'next/router'
 import toast from 'react-hot-toast'
 import { AxiosClient } from '@/lib/axios'
 import pouchMobileLogo from '@/images/pouchlogo-red.svg'
+import { AnalyticsBrowser } from '@segment/analytics-next'
+
+const analytics = AnalyticsBrowser.load({ writeKey: 'F1Q561QsfUIG0c6UiPrdHhIMAobwEFMt' })
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,8 +39,12 @@ export default function Signup() {
         }
     });
 
+    analytics.identify('viewed sign up')
+
     const handleSignup = async(data)=> {
 		try {
+      analytics.identify('completed sign up')
+
       let res = await AxiosClient.post('auth/signup',
       {
           email: data.email,
